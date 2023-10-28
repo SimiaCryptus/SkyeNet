c40ba135c57571e54ad5857ca5dedd97c5d4eda8
Andrew Charneski <acharneski@gmail.com>
2023-10-27 22:17:39 -0400

    1.0.20

--- core/src/main/kotlin/com/simiacryptus/skyenet/prompts/PromptOptimization.kt
+++ core/src/main/kotlin/com/simiacryptus/skyenet/prompts/PromptOptimization.kt
@@ -1,10 +1,19 @@
 package com.simiacryptus.skyenet.prompts
 
-class PromptOptimization {
+import com.simiacryptus.openai.OpenAIClient
+
+class PromptOptimization(
+    val api: OpenAIClient
+) {
     companion object {
         fun main() {
-            // Run the test cases and gather the results
-            runGeneticGeneration(
+            val opt = PromptOptimization(
+                OpenAIClient(
+                    key = "sk-<your key here>",
+                )
+            )
+
+            opt.runGeneticGeneration(
                 seedPrompts = listOf(
                     "Hello, I'm your assistant. How can I help you today?"
                 ), testCases = listOf(
@@ -54,7 +63,7 @@ class PromptOptimization {
                 )
             )
 
-            runGeneticGeneration(
+            opt.runGeneticGeneration(
                 seedPrompts = listOf(
                     """You are a search assistant; you interpret user requests and return search results. 
                         |You are given a user request and respond with a call to `search("query text")`.
@@ -74,64 +83,64 @@ class PromptOptimization {
                 )
             )
         }
+    }
 
-        fun runGeneticGeneration(
-            seedPrompts: List<String>,
-            testCases: List<TestCase>
-        ): List<String> {
-            // For each of N prompts, Run the test cases and assign each prompt a score
-            // Select top log(N) prompts
-            // Recombine and mutate to regenerate N prompts
-            // Repeat until convergence
-            var generation = 0
-            var topPrompts = seedPrompts
-            for (i in 0..100) {
-                val scores = topPrompts.map { prompt ->
-                    prompt to testCases.map { testCase ->
-                        evaluate(prompt, testCase)
-                    }.average()
-                }
-                val survivors = scores.sortedByDescending { it.second }.take(10).map { it.first }
-                topPrompts = regenerate(survivors, 10)
-                println("Generation $generation: ${topPrompts.first()}")
-                generation++
+    fun runGeneticGeneration(
+        seedPrompts: List<String>,
+        testCases: List<TestCase>
+    ): List<String> {
+        // For each of N prompts, Run the test cases and assign each prompt a score
+        // Select top log(N) prompts
+        // Recombine and mutate to regenerate N prompts
+        // Repeat until convergence
+        var generation = 0
+        var topPrompts = seedPrompts
+        for (i in 0..100) {
+            val scores = topPrompts.map { prompt ->
+                prompt to testCases.map { testCase ->
+                    evaluate(prompt, testCase)
+                }.average()
             }
-            return listOf()
+            val survivors = scores.sortedByDescending { it.second }.take(10).map { it.first }
+            topPrompts = regenerate(survivors, 10)
+            println("Generation $generation: ${topPrompts.first()}")
+            generation++
         }
+        return listOf()
+    }
 
-        private fun regenerate(survivors: List<String>, i: Int): List<String> {
-            // Use the recombinator to generate i new prompts from the survivors
-            val result = listOf<String>().toMutableList()
-            result += survivors
-            while (result.size < i) {
-                if(survivors.size == 1) {
-                    result += mutate(survivors.first())
-                    continue
-                } else if(survivors.size == 0) {
-                    throw RuntimeException("No survivors")
-                }
-                val a = survivors.random()
-                val b = survivors.random()
-                result += recombine(a, b)
+    private fun regenerate(survivors: List<String>, i: Int): List<String> {
+        // Use the recombinator to generate i new prompts from the survivors
+        val result = listOf<String>().toMutableList()
+        result += survivors
+        while (result.size < i) {
+            if (survivors.size == 1) {
+                result += mutate(survivors.first())
+                continue
+            } else if (survivors.size == 0) {
+                throw RuntimeException("No survivors")
             }
-            return result
-
+            val a = survivors.random()
+            val b = survivors.random()
+            result += recombine(a, b)
         }
+        return result
 
-        private fun evaluate(systemPrompt: String, testCase: TestCase): Double {
-            // Run the test case and return a score
-            TODO()
-        }
+    }
 
-        private fun mutate(prompt: String): String {
-            // Mutate a prompt
-            TODO()
-        }
+    private fun evaluate(systemPrompt: String, testCase: TestCase): Double {
+        // Run the test case and return a score
+        TODO()
+    }
 
-        private fun recombine(a: String, b: String): String {
-            // Recombine two prompts
-            TODO()
-        }
+    private fun mutate(prompt: String): String {
+        // Mutate a prompt
+        TODO()
+    }
+
+    private fun recombine(a: String, b: String): String {
+        // Recombine two prompts
+        TODO()
     }
 }
 
@@ -141,29 +150,29 @@ data class Turn(val userMessage: String, val expectations: List<Expectation>)
 
 sealed class Expectation {
     class VectorMatch(val content: String) : Expectation() {
-        override fun matches(prompt: String): Boolean {
+        override fun matches(api: OpenAIClient, prompt: String): Boolean {
             TODO("Not yet implemented")
         }
 
-        override fun score(prompt: String): Double {
+        override fun score(api: OpenAIClient, prompt: String): Double {
             TODO("Not yet implemented")
         }
     }
 
     class ContainsMatch(pattern: Regex) : Expectation() {
-        override fun matches(prompt: String): Boolean {
+        override fun matches(api: OpenAIClient, prompt: String): Boolean {
             TODO("Not yet implemented")
         }
 
-        override fun score(prompt: String): Double {
+        override fun score(api: OpenAIClient, prompt: String): Double {
             TODO("Not yet implemented")
         }
 
     }
 
-    abstract fun matches(prompt: String): Boolean
+    abstract fun matches(api: OpenAIClient, prompt: String): Boolean
 
-    abstract fun score(prompt: String): Double
+    abstract fun score(api: OpenAIClient, prompt: String): Double
 
 
 }
