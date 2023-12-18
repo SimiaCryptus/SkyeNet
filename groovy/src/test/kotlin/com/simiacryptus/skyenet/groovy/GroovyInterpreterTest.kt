b3577b587a988b9587c03bcacc8b9a6597e13b22
Andrew Charneski <acharneski@gmail.com>
2023-12-18 01:16:09 -0500

    fixes

--- groovy/src/test/kotlin/com/simiacryptus/skyenet/groovy/GroovyInterpreterTest.kt
+++ groovy/src/test/kotlin/com/simiacryptus/skyenet/groovy/GroovyInterpreterTest.kt
@@ -5,7 +5,10 @@ package com.simiacryptus.skyenet.groovy
 import com.simiacryptus.skyenet.core.util.InterpreterTestBase
 
 class GroovyInterpreterTest : InterpreterTestBase() {
-    override fun newInterpreter(map: java.util.Map<String,Object>) = GroovyInterpreter(map)
+    override fun newInterpreter(map: Map<String,Any>) = GroovyInterpreter(map.map { it.key to it.value as Object }.toMap().toJavaMap())
 
 }
+
+@Suppress("UNCHECKED_CAST")
+private fun <K, V> Map<K, V>.toJavaMap() = HashMap(this) as java.util.Map<K, V>
 
--- scala/src/test/scala/com/simiacryptus/skyenet/scala/ScalaLocalInterpreterTest.scala
+++ scala/src/test/scala/com/simiacryptus/skyenet/scala/ScalaLocalInterpreterTest.scala
@@ -7,7 +7,9 @@ import java.util
 
 
 class ScalaLocalInterpreterTest extends InterpreterTestBase {
-  override def newInterpreter(map: util.Map[String, AnyRef]): Interpreter = {
-    new ScalaLocalInterpreter(map)
-  }
+//  override def newInterpreter(map: util.Map[String, AnyRef]): Interpreter = {
+//    new ScalaLocalInterpreter(map)
+//  }
+
+  override def newInterpreter(map: util.Map[String, _]): Interpreter = new ScalaLocalInterpreter(map.asInstanceOf[util.Map[String, Object]])
 }
