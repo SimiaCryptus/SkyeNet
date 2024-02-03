ca50d98318024db20d5b787782c9e99af24cde76
Andrew Charneski <acharneski@gmail.com>
2024-02-03 17:50:08 -0500

    Update CodingAgent.kt

--- webui/src/main/kotlin/com/simiacryptus/skyenet/apps/coding/CodingAgent.kt
+++ webui/src/main/kotlin/com/simiacryptus/skyenet/apps/coding/CodingAgent.kt
@@ -100,7 +100,7 @@ open class CodingAgent<T : Interpreter>(
   protected open fun displayCode(
     task: SessionTask,
     codeRequest: CodingActor.CodeRequest,
-    response: CodeResult /*= actor.answer(codeRequest, api = api)*/,
+    response: CodeResult,
   ) {
     try {
       task.add(
@@ -128,65 +128,69 @@ open class CodingAgent<T : Interpreter>(
   ) {
     var formText: String? = null
     var formHandle: StringBuilder? = null
-    val playHandler: (t: Unit) -> Unit = {
-      formHandle?.clear()
-      val header = task.header("Running...")
-      var revertButton: StringBuilder? = null
-      try {
-        execute(ui.newTask(), response, request)
-      } finally {
-        header?.clear()
-        revertButton = task.complete(ui.hrefLink("↩", "href-link regen-button") {
-          revertButton?.clear()
-          formHandle?.append(formText)
-          task.complete()
-        })
-      }
-    }
-    val regenHandler: (t: Unit) -> Unit = {
-      formHandle?.clear()
-      val header = task.header("Regenerating...")
-      var revertButton: StringBuilder? = null
-      try {
-        displayCode(ui.newTask(), request.copy(messages = request.messages.dropLastWhile { it.second == ApiModel.Role.assistant }))
-      } finally {
-        header?.clear()
-        revertButton = task.complete(ui.hrefLink("↩", "href-link regen-button") {
-          revertButton?.clear()
-          formHandle?.append(formText)
-          task.complete()
-        })
-      }
-    }
-    val feedbackHandler: (t: String) -> Unit = { feedback ->
-      formHandle?.clear()
-      val header = task.header("Revising...")
-      var revertButton: StringBuilder? = null
-      try {
-        feedback(ui.newTask(), feedback, request, response)
-      } finally {
-        header?.clear()
-        revertButton = task.complete(ui.hrefLink("↩", "href-link regen-button") {
-          revertButton?.clear()
-          formHandle?.append(formText)
-          task.complete()
-        })
-      }
-    }
     formHandle = task.add(
       """
       |<div style="display: flex;flex-direction: column;">
-      |${if (canPlay) ui.hrefLink("▶", "href-link play-button", playHandler) else ""}
-      |${if (canPlay) ui.hrefLink("♻", "href-link regen-button", regenHandler) else ""}
+      |${ if (!canPlay) "" else
+          ui.hrefLink("▶", "href-link play-button") {
+            responseAction(task, "Running...", formHandle, formText) {
+              execute(ui.newTask(), response, request)
+            }
+          }
+      }
+      |${ui.hrefLink("♻", "href-link regen-button") {
+        responseAction(task, "Regenerating...", formHandle, formText) {
+          displayCode(
+            ui.newTask(),
+            request.copy(messages = request.messages.dropLastWhile { it.second == ApiModel.Role.assistant })
+          )
+        }
+      }}
       |</div>
-      |${ui.textInput(feedbackHandler)}
+      |${ui.textInput { feedback ->
+        responseAction(task, "Revising...", formHandle, formText) {
+          feedback(ui.newTask(), feedback, request, response)
+        }
+      }
+      }
     """.trimMargin(), className = "reply-message"
     )
     formText = formHandle.toString()
     task.complete()
   }
 
-  private fun feedback(
+  protected open fun responseAction(
+    task: SessionTask,
+    message: String,
+    formHandle: StringBuilder?,
+    formText: String?,
+    fn: () -> Unit = {}
+  ) {
+    formHandle?.clear()
+    val header = task.header(message)
+    try {
+      fn()
+    } finally {
+      header?.clear()
+      revertButton(task, formHandle, formText)
+    }
+  }
+
+  protected open fun revertButton(
+    task: SessionTask,
+    formHandle: StringBuilder?,
+    formText: String?
+  ): StringBuilder? {
+    var revertButton: StringBuilder? = null
+    revertButton = task.complete(ui.hrefLink("↩", "href-link regen-button") {
+      revertButton?.clear()
+      formHandle?.append(formText)
+      task.complete()
+    })
+    return revertButton
+  }
+
+  protected open fun feedback(
     task: SessionTask,
     feedback: String,
     request: CodingActor.CodeRequest,
@@ -207,7 +211,7 @@ open class CodingAgent<T : Interpreter>(
     }
   }
 
-  private fun execute(
+  protected open fun execute(
     task: SessionTask,
     response: CodeResult,
     request: CodingActor.CodeRequest
