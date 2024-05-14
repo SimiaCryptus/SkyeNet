b91c89a6ad78900075f022c0aa487a102b352e91
Andrew Charneski <acharneski@gmail.com>
2024-05-14 16:56:53 -0500

    1.0.70 (#75)

--- webui/src/main/resources/application/tabs.js
+++ webui/src/main/resources/application/tabs.js
@@ -4,47 +4,63 @@ function updateTabs() {
             event.stopPropagation();
             const forTab = button.getAttribute('data-for-tab');
             let tabsParent = button.closest('.tabs-container');
-            tabsParent.querySelectorAll('.tab-button').forEach(tabButton => tabButton.classList.remove('active'));
+            tabsParent.querySelectorAll('.tab-button').forEach(tabButton => {
+                if (tabButton.closest('.tabs-container') === tabsParent) tabButton.classList.remove('active')
+            });
             button.classList.add('active');
+            let selectedContent = null;
             tabsParent.querySelectorAll('.tab-content').forEach(content => {
-                const contentParent = content.closest('.tabs-container');
-                if (contentParent === tabsParent) {
+                if (content.closest('.tabs-container') === tabsParent) {
                     if (content.getAttribute('data-tab') === forTab) {
                         content.classList.add('active');
-                       content.style.display = 'block'; // Ensure the content is displayed
+                        content.style.display = 'block'; // Ensure the content is displayed
+                        selectedContent = content;
                     } else {
                         content.classList.remove('active')
-                       content.style.display = 'none'; // Ensure the content is hidden
+                        content.style.display = 'none'; // Ensure the content is hidden
                     }
                 }
             });
-           // Ensure nested tabs are updated
-           updateNestedTabs(tabsParent);
+            if (selectedContent !== null) updateNestedTabs(selectedContent);
         })
     });
 }
 
-function updateNestedTabs(tabsParent) {
-    tabsParent.querySelectorAll('.tab-content .tabs-container').forEach(nestedTabsContainer => {
-        const firstNestedButton = nestedTabsContainer.querySelector('.tab-button');
-        if (firstNestedButton) {
-            firstNestedButton.click();
+function updateNestedTabs(element) {
+    element.querySelectorAll('.tabs-container').forEach(tabsContainer => {
+        try {
+            let hasActiveButton = false;
+            tabsContainer.querySelectorAll('.tab-button').forEach(nestedButton => {
+                if (nestedButton.classList.contains('active')) {
+                    hasActiveButton = true;
+                }
+            });
+            if (!hasActiveButton) {
+                /* Determine if a tab-content element in this tabs-container has the active class. If so, use its data-tab value to find the matching button and ensure it is marked active */
+                const activeContent = tabsContainer.querySelector('.tab-content.active');
+                if (activeContent) {
+                    const activeTab = activeContent.getAttribute('data-tab');
+                    const activeButton = tabsContainer.querySelector(`.tab-button[data-for-tab="${activeTab}"]`);
+                    if (activeButton !== null) {
+                        activeButton.classList.add('active');
+                    }
+                } else {
+                    /* Add 'active' to the class list of the first button */
+                    const firstButton = tabsContainer.querySelector('.tab-button');
+                    if (firstButton !== null) {
+                        firstButton.classList.add('active');
+                    }
+                }
+            }
+        } catch (e) {
+            console.log("Error updating tabs: " + e);
         }
     });
 }
+
 document.addEventListener('DOMContentLoaded', () => {
-    try {
-        updateTabs();
-       // Set the initial active tab
-       document.querySelectorAll('.tabs-container').forEach(tabsContainer => {
-           const firstButton = tabsContainer.querySelector('.tab-button');
-           if (firstButton) {
-               firstButton.click();
-           }
-       });
-    } catch (e) {
-        console.log("Error updating tabs: " + e);
-    }
+    updateTabs();
+    updateNestedTabs(document);
 });
 
 window.updateTabs = updateTabs; // Expose updateTabs to the global scope
