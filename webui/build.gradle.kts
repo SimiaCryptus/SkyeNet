8d889235cc1edf4cbb62100b3f18525d067744ff
Andrew Charneski <acharneski@gmail.com>
2024-06-21 11:00:09 -0400

    1.0.61

--- webui/build.gradle.kts
+++ webui/build.gradle.kts
@@ -35,7 +35,7 @@ val jetty_version = "11.0.18"
 val jackson_version = "2.17.0"
 dependencies {
 
-    implementation(group = "com.simiacryptus", name = "jo-penai", version = "1.0.62")
+    implementation(group = "com.simiacryptus", name = "jo-penai", version = "1.0.61")
 
     implementation(project(":core"))
     implementation(project(":kotlin"))
