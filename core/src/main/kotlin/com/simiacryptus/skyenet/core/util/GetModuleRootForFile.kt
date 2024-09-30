004eb49417eaa2e26f15b5f8ee88a9c4eed3b8d7
Andrew Charneski <acharneski@gmail.com>
2024-09-30 00:17:52 -0400

    fixes

--- core/src/main/kotlin/com/simiacryptus/skyenet/core/util/GetModuleRootForFile.kt
+++ core/src/main/kotlin/com/simiacryptus/skyenet/core/util/GetModuleRootForFile.kt
@@ -3,12 +3,15 @@ package com.simiacryptus.skyenet.core.util
 import java.io.File
 
 fun getModuleRootForFile(file: File): File {
+    if (file.isFile) {
+        return getModuleRootForFile(file.parentFile)
+    }
     var current = file
-    while (current.parentFile != null) {
+    do {
         if (current.resolve(".git").exists()) {
             return current
         }
-        current = current.parentFile
-    }
+        current = current.parentFile ?: break
+    } while (true)
     return file
 }
